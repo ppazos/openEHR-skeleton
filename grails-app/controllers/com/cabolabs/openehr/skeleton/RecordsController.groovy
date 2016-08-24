754e9f53961bbb8fbcd7270b2ebc1df5aa7e394c
Pablo Pazos <pablo.pazos@cabolabs.com>
2016-08-24 03:41:05 -0300

    added dependencies to process openEHR archetypes

--- grails-app/controllers/com/cabolabs/openehr/skeleton/RecordsController.groovy
+++ grails-app/controllers/com/cabolabs/openehr/skeleton/RecordsController.groovy
@@ -1,6 +1,11 @@
 package com.cabolabs.openehr.skeleton
 
+import com.cabolabs.openehr.adl.ArchetypeManager
+
 class RecordsController {
+
+    private static String PS = File.separator
+    private static String path = "."+ PS +"archetypes"
 
     def index()
     {
@@ -13,6 +18,14 @@ class RecordsController {
     def save_blood_pressure()
     {
        println params
+       
+       def loader = ArchetypeManager.getInstance(path)
+       loader.loadAll()
+       
+       def archetype = loader.getArchetype(params.archetypeId)
+       
+       assert archetype.archetypeId.value == params.archetypeId
+       
        redirect action: 'create_blood_pressure'
     }
 }
